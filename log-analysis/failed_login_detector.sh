d366a5fa517f7029eda632ff189fdcc46ae15df8
Eseigbe Ihinosen <eseigbeihinosen@gmail.com>
2026-01-14 15:59:59 +0100

    Fix IP extraction logic in failed login detector

--- log-analysis/failed_login_detector.sh
+++ log-analysis/failed_login_detector.sh
@@ -8,7 +8,7 @@ THRESHOLD=5
 echo "Detecting IPs with more than $THRESHOLD failed login attempts..."
 
 grep "Failed password" "$LOG_FILE" \
-| awk '{print $(NF-3)}' \
+| awk '{for (i=1;i<=NF;i++) if ($i=="from") print $(i+1)}' \
 | sort \
 | uniq -c \
 | awk -v t="$THRESHOLD" '$1 >= t {print $2 " - " $1 " failed attempts"}'
